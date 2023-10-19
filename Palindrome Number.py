b243f6dfdbd7a701fe53a2c0168f08e3bc580534
vijay-lingam <148071420+vijay-lingam@users.noreply.github.com>
2023-10-19 21:59:02 +0530

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -6,3 +6,19 @@ class Solution:
             return True
         else:
             return False
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        temp = x
+        res = 0
+
+        while(temp != 0):
+            digit = temp % 10 
+            res = res * 10 + digit 
+            temp = temp // 10
+
+        return x == res 
